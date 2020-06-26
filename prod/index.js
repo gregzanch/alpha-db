3c14405da10b9066a8cec46f2fcba4fd001cf9b6
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-06-26 02:53:19 -0400

    change dev scripts

--- prod/index.js
+++ prod/index.js
@@ -5,11 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const server_1 = __importDefault(require("./server"));
+const rootDir = process.cwd();
 const server = new server_1.default();
-const cwd = process.cwd();
-console.log(cwd);
 server.app.get('/', (req, res) => {
-    res.sendFile(path_1.default.resolve(__dirname, "../", "index.html"));
+    res.sendFile(path_1.default.resolve(rootDir, "index.html"));
 });
 server.connect();
 process.on("SIGINT", function () {
