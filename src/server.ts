1a8251cec0ca2ac9f49748eae57e23c3ea81cd45
Greg Zanchelli <zanchelli.greg@gmail.com>
2021-01-21 13:43:13 -0500

    adjust env variables

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,9 @@ import bodyParser from "body-parser";
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import fetch from "node-fetch";
-import env, { Env } from './env';
-import to from './util/to';
+import to from "./util/to";
 import MaterialSchema from "./material-schema";
-import * as MaterialMethods from './material-methods';
-
+import * as MaterialMethods from "./material-methods";
 
 export interface ServerProps {
   name?: string;
@@ -25,45 +23,43 @@ export class Server {
     this.stop = this.stop.bind(this);
     this.started = false;
   }
-  connect(){
-    mongoose.connect(env.CONNECTION_URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			auth: {
-				user: env.USER,
-				password: env.PASSWORD,
-			},
-		});
-		this.db = mongoose.connection;
-		this.db.on("error", function (err) {
-			console.log(
-				"Mongoose default connection has occured " + err + " error"
-			);
-		});
+  connect() {
+    mongoose.connect(process.env.CONNECTION_URL!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      auth: {
+        user: process.env.USER!,
+        password: process.env.PASSWORD!,
+      },
+    });
+    this.db = mongoose.connection;
+    this.db.on("error", function (err) {
+      console.log("Mongoose default connection has occured " + err + " error");
+    });
 
-		this.db.on("connected", function () {
-			console.log("Mongoose default connection is open");
-		});
+    this.db.on("connected", function () {
+      console.log("Mongoose default connection is open");
+    });
 
-		this.db.on("disconnected", function () {
-			console.log("Mongoose default connection is disconnected");
-		});
+    this.db.on("disconnected", function () {
+      console.log("Mongoose default connection is disconnected");
+    });
 
-		this.db.once("open", this.setEndpoints);
+    this.db.once("open", this.setEndpoints);
   }
   private setEndpoints() {
-    this.app.use(express.static(path.join(process.cwd(), 'public')));
-    this.app.get('/', (req, res) => {
+    this.app.use(express.static(path.join(process.cwd(), "public")));
+    this.app.get("/", (req, res) => {
       res.sendFile(path.join(process.cwd(), "public", "index.html"));
-    })
-    this.app.get('/api/material/all', MaterialMethods.getAll);
-    this.app.get('/api/material/brief', MaterialMethods.getBrief);
-    this.app.get('/api/material/find', MaterialMethods.find);
+    });
+    this.app.get("/api/material/all", MaterialMethods.getAll);
+    this.app.get("/api/material/brief", MaterialMethods.getBrief);
+    this.app.get("/api/material/find", MaterialMethods.find);
   }
   start() {
     if (!this.started) {
-      this.app.listen(env.PORT, env.HOST, () => {
-        console.log("http://%s:%s", env.HOST, env.PORT);
+      this.app.listen(Number(process.env.PORT)!, process.env.HOST!, () => {
+        console.log("http://%s:%s", process.env.HOST!, process.env.PORT!);
       });
     }
   }
@@ -71,8 +67,8 @@ export class Server {
   stop() {
     this.db.close(() => {
       console.log("connection disconnected");
-    });			
-	}
+    });
+  }
 }
 
 export default Server;
